b2234908b3295b0801dfabb0e7dae6ab657dd980
konstantinos katrioplas <konst.katrioplas@gmail.com>
2017-11-17 20:25:14 +0100

    new doc

--- CGAL/doc_output/Polygon_mesh_processing/group__PMP__meshing__grp.js
+++ CGAL/doc_output/Polygon_mesh_processing/group__PMP__meshing__grp.js
@@ -9,7 +9,7 @@ var group__PMP__meshing__grp =
     [ "smooth_angles", "group__PMP__meshing__grp.html#ga34ac2571e89a2cf2ecd4e222d4c3a6a9", null ],
     [ "smooth_areas", "group__PMP__meshing__grp.html#ga7895765f5bc5a44017beec0a9e3379d1", null ],
     [ "smooth_curvature_flow", "group__PMP__meshing__grp.html#gaf12091d15cf839363e34bba7a93996a9", null ],
-    [ "smooth_modified_curvature_flow", "group__PMP__meshing__grp.html#gad48a6a3a292384dda8bba3db2d25dee0", null ],
+    [ "smooth_modified_curvature_flow", "group__PMP__meshing__grp.html#ga1579cbc64d1f8333ee6e2ef85cb4b4ee", null ],
     [ "split_long_edges", "group__PMP__meshing__grp.html#gaafd017f4424c3942bfdcc93874c8f596", null ],
     [ "triangulate_face", "group__PMP__meshing__grp.html#ga70d65044f8c7309c24ade88fa280124a", null ],
     [ "triangulate_faces", "group__PMP__meshing__grp.html#gacaaff4d520500c530d9c3d5ebe2a0760", null ],
