cb474da8a34ccbcd01dc62cc2a089505d7cb99ba
konstantinos katrioplas <konst.katrioplas@gmail.com>
2017-11-17 16:35:29 +0100

    manual

--- CGAL/doc_output/Polygon_mesh_processing/group__PMP__meshing__grp.js
+++ CGAL/doc_output/Polygon_mesh_processing/group__PMP__meshing__grp.js
@@ -1,14 +1,15 @@
 var group__PMP__meshing__grp =
 [
-    [ "angle_smoothing", "group__PMP__meshing__grp.html#ga2050732e1d64ed32149eb998501e7831", null ],
-    [ "area_smoothing", "group__PMP__meshing__grp.html#ga4ebffb6c537f594843b588dc1b91ca03", null ],
     [ "compatible_smoothing", "group__PMP__meshing__grp.html#ga54c04b43e8db74366a896de8f4b96424", null ],
     [ "fair", "group__PMP__meshing__grp.html#gaa091c8368920920eed87784107d68ecf", null ],
     [ "isotropic_remeshing", "group__PMP__meshing__grp.html#gad3d03890515ae8103bd32a30a3486412", null ],
     [ "random_perturbation", "group__PMP__meshing__grp.html#ga028a80dc84395650f67714fa7618ec53", null ],
     [ "random_perturbation", "group__PMP__meshing__grp.html#gac53b0a32bc4ca2bb220ec47bb241d7e5", null ],
     [ "refine", "group__PMP__meshing__grp.html#gad0449d8e1021fc46507074cd6db65ef4", null ],
-    [ "shape_smoothing", "group__PMP__meshing__grp.html#ga7a49e1f7c0c3fa63f8ee521a7687ebd0", null ],
+    [ "smooth_angles", "group__PMP__meshing__grp.html#ga34ac2571e89a2cf2ecd4e222d4c3a6a9", null ],
+    [ "smooth_areas", "group__PMP__meshing__grp.html#ga7895765f5bc5a44017beec0a9e3379d1", null ],
+    [ "smooth_curvature_flow", "group__PMP__meshing__grp.html#gaf12091d15cf839363e34bba7a93996a9", null ],
+    [ "smooth_modified_curvature_flow", "group__PMP__meshing__grp.html#gad48a6a3a292384dda8bba3db2d25dee0", null ],
     [ "split_long_edges", "group__PMP__meshing__grp.html#gaafd017f4424c3942bfdcc93874c8f596", null ],
     [ "triangulate_face", "group__PMP__meshing__grp.html#ga70d65044f8c7309c24ade88fa280124a", null ],
     [ "triangulate_faces", "group__PMP__meshing__grp.html#gacaaff4d520500c530d9c3d5ebe2a0760", null ],
